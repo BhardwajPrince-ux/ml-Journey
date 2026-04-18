cb55fb442978a988efaec4733091d99c974aaa9c
Prince Bhardwaj <bhardwajprince332@gmail.com>
2026-04-18 13:16:39 +0530

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,7 @@
+#DAY 1 - ML Journey Start
+import numpy as np
+arr= np.array([1,2,3,4,5])
+print("My first Array:", arr)
+print("Sum:", np.sum(arr))
+print("Avg:", np.mean(arr))
+print("Max:", np.max(arr))
